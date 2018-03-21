e541c39f3babdd3baa724b1483bc119b6106a93c
Adriana Cuellas <32887126+acuellas@users.noreply.github.com>
2018-03-21 09:56:15 -0400

    Initial commit

---
+++ COMP305_Lab5/Assets/Scripts/ScoreCalculate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCalculate : MonoBehaviour {
+
+    public GameObject persistedObject;
+    // Use this for initialization
+    void Start () {
+        Instantiate(persistedObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Awake()
+    {
+        persistedObject = GameObject.Find("Score");
+    }
+}
